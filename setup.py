23ec30270549ff8349ec4b48156ed8eb7d217a24
dragdev <samueljohnnicholson@gmail.com>
2020-09-17 20:46:32 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+try:
+    with open("./README.md") as rfile:
+        long = rfile.read()
+except:
+    long = ""
+
+setup(
+    name='SBLPy',
+    version='0.9.1',
+    packages=['sblpy'],
+    url='https://github.com/EEKIM10/sblpy',
+    license='MIT',
+    author='EEKIM10',
+    author_email='eek@clicksminuteper.net',
+    description='SBLP\'s HTTP system, wrapped into python',
+    long_description=long,
+    long_description_content_type='text/markdown',
+)
